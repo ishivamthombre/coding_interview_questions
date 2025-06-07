8567717b704996910c127b23ca43e0d1300712eb
Shivam Thombre <ishivamthombre@gmail.com>
2025-06-07 20:16:51 +0530

    added fibonacci series using recursion

---
+++ coding_questions/src/FibonacciRecursive.java
@@ -0,0 +1,21 @@
+public class FibonacciRecursive {
+
+  // Recursive method to get nth Fibonacci number
+  public static int fibonacci(int n) {
+    if (n == 0)
+      return 0;       // base case 0th number
+    if (n == 1)
+      return 1;       // base case 1st number
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  public static void main(String[] args) {
+    int count = 15;
+    System.out.println("First " + count + " Fibonacci numbers:");
+
+    for (int i = 0; i < count; i++) {
+      System.out.print(fibonacci(i) + " ");
+    }
+  }
+}
+
